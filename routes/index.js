cfffad7fc3adb31c0451da08ba9ac0c971246f06
Hussain Thajutheen <Hussain Thajutheen>
2016-12-21 17:49:50 +0530

    Implemented CSurf and Session

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find((err, docs) => {
     var productChunks =[];
     var chunkSize = 3;
-    for (var i =0; i <docs.length; i += chunkSize) {
+    for (var i = 0; i < docs.length; i += chunkSize) {
       productChunks.push(docs.slice(i, i + chunkSize));
     }
     res.render('shop/index', { title: 'Shopping Cart', heading: "Hello", products: productChunks });
   });
+});
+
+router.get('/user/signup', (req, res, next) => {
+  res.render('user/signup', {csrfToken: req.csrfToken()});
+});
+
+router.post('/user/signup', (req, res, next) => {
+  res.redirect('/');
 });
 
 module.exports = router;

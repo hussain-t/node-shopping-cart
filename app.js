5688c8245585b366d2bddb7ab1fe38627f71715e
أبو عبد الله حسين <   >
2016-12-26 21:19:38 +0530

    Successfully implemented Passport and bug for signup redirect

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 //DB setup
 mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
@@ -27,11 +29,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret: 'mysecretcode', resave: false, saveUninitialized: false}));
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
